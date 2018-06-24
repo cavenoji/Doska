ed7baf736455e8d39600fe24d5782525cb9da677
cavenoji <ickpanah@gmail.com>
2018-06-24 13:49:06 +0300

    Changed responce schema

--- libs/getblobs.js
+++ libs/getblobs.js
@@ -2,9 +2,9 @@
 
 module.exports = (blobs) => {
     blobs.map(blob => {
-        blob.photo.large = "/static/photos/large/" + blob.photo.large.split("/").pop();
-        blob.photo.normal = "/static/photos/compressed/" + blob.photo.normal.split("/").pop();
-        blob.photo.thumbnail = "/static/photos/downscaled/" + blob.photo.thumbnail.split("/").pop();
+        blob.photo.large = "/static/photos/" + blob.photo.large.split("/").pop();
+        blob.photo.normal = "/static/photos/" + blob.photo.normal.split("/").pop();
+        blob.photo.thumbnail = "/static/photos/" + blob.photo.thumbnail.split("/").pop();
     })
     return blobs;
 }
