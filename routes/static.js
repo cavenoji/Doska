51d6e06b8b926856539a7516715bb1e23341f47e
cavenoji <ickpanah@gmail.com>
2018-06-25 19:40:59 +0300

    Added new path for static files

--- routes/static.js
+++ routes/static.js
@@ -3,39 +3,28 @@
 let express = require('express');
 let router = express.Router();
 let fs = require('fs');
-const filesPath = '/home/cavenoj/doska-v2-auth/pics/';
+let validateFile = require('../libs/validatefile');
+
+const adsFilesPath = [__dirname, '..', 'pics', ''].join('/');
+const categoriesThumbnailsPath = [__dirname, '..', 'config', 'categories', ''].join('/');
+
 
 router
     .param('name', function(req, res, next, name){
-        fs.stat(filesPath + name, (err, stats) => {
-            if (err) {
-                console.error(`Photo ${name} was not found`);
-                res.status(404)
-                let err = new Error('Not Found');
-                err.status = 404;
-                res.format({
-                    html: function(){
-                        next(err);
-                    },
-                    json: function(){
-                        res.status(err.status).json({message : ' ' + err});
-                    }
-                });
-            //if it is found we continue on
-            } 
-            else {
-                //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
-                console.log(`Photo ${name} was found`);
-                // once validation is done save the new item in the req
-                req.name = filesPath + name;
-                // go to the next thing
-                next(); 
-            }
-        })
+        name = adsFilesPath + name;
+        validateFile(req, res, next, name);
+    })
+    .param('thumbnail', function(req, res, next, thumbnail){
+        thumbnail = categoriesThumbnailsPath + thumbnail;
+        validateFile(req, res, next, thumbnail);
     });
 
 router
     .get('/photos/:name', function(req, res, next){
+        res.status(200).download(req.name);
+    });
+router
+    .get('/photos/categories/:thumbnail', function(req, res, next){
         res.status(200).download(req.name);
     });
 
